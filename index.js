1f8a9e3ee625c7d265de1e0446297d66f7e4f7bb
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-18 13:41:14 +0200

    inital commit

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
+var Instapaper = require('instapaper'),
+    _ = require('lodash'),
+    util = require('./util.js'),
+    apiUrl = 'https://www.instapaper.com/api/1.1';
+
+var pickOutputs = {
+        '-': {
+            keyName: 'folders',
+            fields: {
+                'folder_id': 'folder_id',
+                'title': 'title'
+            }
+        }
+    };
+
 module.exports = {
+
+    /**
+     * Authorize module.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authModule: function (dexter) {
+        var auth = {},
+            consumerKey = dexter.environment('instapaper_consumer_key'),
+            consumerSecret = dexter.environment('instapaper_consumer_secret'),
+
+            username = dexter.environment('instapaper_username'),
+            password = dexter.environment('instapaper_password');
+
+        if (consumerKey && consumerSecret && username && password) {
+            auth.consumerKey = consumerKey;
+            auth.consumerSecret = consumerSecret;
+            auth.user = username;
+            auth.pass = password;
+
+        } else {
+            this.fail('A [instapaper_consumer_key, instapaper_consumer_secret, instapaper_username, instapaper_password] environment need for this module.');
+        }
+        
+        return _.isEmpty(auth)? false : auth;
+    },
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +48,21 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var auth = this.authModule(dexter),
+            client = Instapaper(auth.consumerKey, auth.consumerSecret, {apiUrl: apiUrl});
+
+        if (!auth)
+            return;
+
+        client.setUserCredentials(auth.user, auth.pass);
+        client.bookmarks.client.request('/folders/list').then(function (folders) {
+
+            this.complete(util.pickResult({ folders: folders }, pickOutputs));
+
+        }.bind(this)).catch(function(err) {
+            this.fail(err);
+
+        }.bind(this));
     }
 };
