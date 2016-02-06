adcdea1c2de7d9e1dbf3e6b0750148772626d6a7
Daniel Ilkovich <daniel+github@betaworks.com>
2016-02-06 16:06:29 -0500

    getting module ready for prod, update to support providers

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-var Instapaper = require('instapaper'),
-    _ = require('lodash'),
-    util = require('./util.js'),
-    apiUrl = 'https://www.instapaper.com/api/1.1';
+var Instapaper = require('instapaper')
+  , _          = require('lodash')
+  , util       = require('./util.js')
+  , apiUrl     = 'https://www.instapaper.com/api/1.1'
+;
 
 var pickOutputs = {
-        '-': {
-            keyName: 'folders',
-            fields: {
-                'folder_id': 'folder_id',
-                'title': 'title'
-            }
+    '-': {
+        keyName: 'folders',
+        fields: {
+            'folder_id': 'folder_id',
+            'title': 'title'
         }
-    };
+    }
+};
 
 module.exports = {
-
-    /**
-     * Authorize module.
-     *
-     * @param dexter
-     * @returns {*}
-     */
-    authModule: function (dexter) {
-        var auth = {},
-            consumerKey = dexter.environment('instapaper_consumer_key'),
-            consumerSecret = dexter.environment('instapaper_consumer_secret'),
-
-            username = dexter.environment('instapaper_username'),
-            password = dexter.environment('instapaper_password');
-
-        if (consumerKey && consumerSecret && username && password) {
-            auth.consumerKey = consumerKey;
-            auth.consumerSecret = consumerSecret;
-            auth.user = username;
-            auth.pass = password;
-
-        } else {
-            this.fail('A [instapaper_consumer_key, instapaper_consumer_secret, instapaper_username, instapaper_password] environment need for this module.');
-        }
-        
-        return _.isEmpty(auth)? false : auth;
-    },
     /**
      * The main entry point for the Dexter module
      *
@@ -48,21 +22,20 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var auth   = dexter.provider('instapaper').credentials() 
+          , client = Instapaper(auth.consumer_key, auth.consumer_secret, {apiUrl: apiUrl})
+          , self   = this
+        ;
 
-        var auth = this.authModule(dexter),
-            client = Instapaper(auth.consumerKey, auth.consumerSecret, {apiUrl: apiUrl});
+        if (!auth) return self.fail('No instapaper auth credentials provided');
 
-        if (!auth)
-            return;
-
-        client.setUserCredentials(auth.user, auth.pass);
-        client.bookmarks.client.request('/folders/list').then(function (folders) {
-
-            this.complete(util.pickResult({ folders: folders }, pickOutputs));
-
-        }.bind(this)).catch(function(err) {
-            this.fail(err);
-
-        }.bind(this));
+        client.setOAuthCredentials(auth.access_token, auth.access_token_secret);
+        client.bookmarks.client.request('/folders/list')
+            .then(function (folders) {
+                self.complete(util.pickResult({ folders: folders }, pickOutputs));
+            })
+            .catch(function(err) {
+                self.fail(err);
+            });
     }
 };
